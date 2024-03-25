1d8e5f7634afb51fddd7e6e5e9090fdde86b848d
roeppeldoeppel <michakl3000@gmail.com>
2024-03-25 09:12:00 +0100

    showing Images

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {MyButton} from './MyButton'
+import { ShowingImages } from './ShowingImages'
+import spaceImg1 from "./assets/images/spaceImg1.jpg";
+import spaceImg2 from "./assets/images/spaceImg2.jpg";
+import spaceImg3 from "./assets/images/spaceImg3.jpg";
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +34,17 @@ function App() {
       <div className="button">
         <h3>Press the Button</h3>
         <MyButton />
+      </div>
+      <div className="images">
+        <div className="img1">
+          {ShowingImages(spaceImg1)};
+        </div>
+        <div className="img2">
+        {ShowingImages(spaceImg2)};
+        </div>
+        <div className="img3">
+        {ShowingImages(spaceImg3)};
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
